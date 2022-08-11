ab4db770be90407d3a92d80d705c6f51c420f3ef
Danilo Ribeiro <danriba@gmail.com>
2022-08-11 00:49:58 -0300

    Final changes

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -1,13 +1,16 @@
 package com.api.parkingcontrol;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApplication {
+	Logger logger = org.apache.logging.log4j.LogManager.getLogger(ParkingControlApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
 	}
-
 }
